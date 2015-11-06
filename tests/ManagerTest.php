6385d55e163cfd8d56d30f1a0af345d1c0d90c86
brazorf <brazorf@users.noreply.github.com>
2015-11-06 14:32:22 +0100

    chore [tests] style fixes.

--- tests/ManagerTest.php
+++ tests/ManagerTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace Foothing\Wrappr\Tests;
 
 use Foothing\Wrappr\Routes\Route;
 use Foothing\Wrappr\Tests\Mocks\Mocks;
@@ -10,7 +10,10 @@ use Foothing\Wrappr\Tests\Mocks\Routes;
  */
 class ManagerTest extends \PHPUnit_Framework_TestCase  {
 
-    protected $manager, $routes, $users, $permissions;
+    protected $manager;
+    protected $routes;
+    protected $users;
+    protected $permissions;
 
     public function setUp() {
         parent::setUp();
@@ -24,50 +27,49 @@ class ManagerTest extends \PHPUnit_Framework_TestCase  {
         $this->users->shouldReceive('getAuthUser')->once()->andReturn(Mocks::user());
         $this->users->shouldReceive('isSuperAdmin')->once()->andReturn(true);
         $route = new Route(['verb' => 'get', 'pattern' => 'test']);
-        $this->assertTrue( $this->manager->check($route) );
+        $this->assertTrue($this->manager->check($route));
     }
 
     public function test_check_fails_for_unauth_users() {
-        $this->users->shouldReceive('getAuthUser')->andReturn( null );
+        $this->users->shouldReceive('getAuthUser')->andReturn(null);
         $route = new Route(['verb' => 'get', 'pattern' => 'test']);
-        $this->assertFalse( $this->manager->check($route) );
+        $this->assertFalse($this->manager->check($route));
     }
 
     public function test_check_returns_true() {
-        $this->users->shouldReceive('getAuthUser')->andReturn( Mocks::user() );
+        $this->users->shouldReceive('getAuthUser')->andReturn(Mocks::user());
         $this->users->shouldReceive('isSuperAdmin')->once()->andReturn(false);
         $this->permissions->shouldReceive('check')->once()->andReturn(true);
         $route = new Route(['verb' => 'get', 'pattern' => 'test']);
-        $this->assertTrue( $this->manager->check($route) );
+        $this->assertTrue($this->manager->check($route));
     }
 
     public function test_check_returns_false() {
-        $this->users->shouldReceive('getAuthUser')->andReturn( Mocks::user() );
+        $this->users->shouldReceive('getAuthUser')->andReturn(Mocks::user());
         $this->users->shouldReceive('isSuperAdmin')->once()->andReturn(false);
         $this->permissions->shouldReceive('check')->once()->andReturn(false);
         $route = new Route(['verb' => 'get', 'pattern' => 'test']);
-        $this->assertFalse( $this->manager->check($route) );
+        $this->assertFalse($this->manager->check($route));
     }
 
     public function test_check_passes_if_route_not_found() {
-        $this->users->shouldReceive('getAuthUser')->andReturn( null );
-        $this->routes->shouldReceive('paginate')->andReturn( null );
-        $this->assertTrue( $this->manager->checkPath('get', 'test') );
+        $this->users->shouldReceive('getAuthUser')->andReturn(null);
+        $this->routes->shouldReceive('paginate')->andReturn(null);
+        $this->assertTrue($this->manager->checkPath('get', 'test'));
     }
 
     public function testBestMatch() {
-        $this->users->shouldReceive('getAuthUser')->andReturn( null );
-        $this->routes->shouldReceive('paginate')->andReturn( new Routes() );
-        $this->assertEquals(".*", $this->manager->bestMatch('get', 'test')->pattern );
-        $this->assertEquals("api/.*", $this->manager->bestMatch('get', 'api/1')->pattern );
-        $this->assertEquals("api/.*", $this->manager->bestMatch('get', 'api/a')->pattern );
-        $this->assertEquals("api/.*", $this->manager->bestMatch('get', 'api/v')->pattern );
-        $this->assertEquals("api/v1", $this->manager->bestMatch('get', 'api/v1')->pattern );
-        $this->assertEquals("api/v1", $this->manager->bestMatch('get', 'api/v1/')->pattern );
-        $this->assertEquals("api/v1/.*", $this->manager->bestMatch('get', 'api/v1/whatever/1')->pattern );
-        $this->assertEquals("api/v1/.*", $this->manager->bestMatch('get', 'api/v1/users/')->pattern );
-        $this->assertEquals("api/v1/users/[0-9]+", $this->manager->bestMatch('get', 'api/v1/users/10')->pattern );
-        $this->assertEquals("api/v1/users/[0-9]+/.*", $this->manager->bestMatch('get', 'api/v1/users/10/whatever')->pattern );
+        $this->users->shouldReceive('getAuthUser')->andReturn(null);
+        $this->routes->shouldReceive('paginate')->andReturn(new Routes());
+        $this->assertEquals(".*", $this->manager->bestMatch('get', 'test')->pattern);
+        $this->assertEquals("api/.*", $this->manager->bestMatch('get', 'api/1')->pattern);
+        $this->assertEquals("api/.*", $this->manager->bestMatch('get', 'api/a')->pattern);
+        $this->assertEquals("api/.*", $this->manager->bestMatch('get', 'api/v')->pattern);
+        $this->assertEquals("api/v1", $this->manager->bestMatch('get', 'api/v1')->pattern);
+        $this->assertEquals("api/v1", $this->manager->bestMatch('get', 'api/v1/')->pattern);
+        $this->assertEquals("api/v1/.*", $this->manager->bestMatch('get', 'api/v1/whatever/1')->pattern);
+        $this->assertEquals("api/v1/.*", $this->manager->bestMatch('get', 'api/v1/users/')->pattern);
+        $this->assertEquals("api/v1/users/[0-9]+", $this->manager->bestMatch('get', 'api/v1/users/10')->pattern);
+        $this->assertEquals("api/v1/users/[0-9]+/.*", $this->manager->bestMatch('get', 'api/v1/users/10/whatever')->pattern);
     }
-
 }
